0572840012c57a17f78f0604ca28957432bd7ad6
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-19 20:12:23 +0330

    Style the active link in the navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import { GiSandSnake } from "react-icons/gi";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 export default function NavBar() {
   // list of nav links:
@@ -7,6 +11,8 @@ export default function NavBar() {
     { id: 1, href: "/", content: "Dashboard" },
     { id: 2, href: "/issue", content: "Issues" },
   ];
+  // get path of url:
+  const currentPath = usePathname();
   return (
     <nav className="flex gap-6 text-xl mb-5 px-5 border-b h-14 items-center">
       <Link href="#">
@@ -16,7 +22,11 @@ export default function NavBar() {
         {links.map((link) => (
           <Link
             key={link.id}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": currentPath === link.href,
+              "text-zinc-500": currentPath !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.content}
