8e5dfae3ad1f989d1a73788d8714bc031b9ece39
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-20 16:21:14 +0330

    Customize Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
 import "@radix-ui/themes/styles.css";
+import "./ui/theme-config.css";
 import type { Metadata } from "next";
-import "./globals.css";
+import "./ui/globals.css";
 import NavBar from "./components/NavBar";
 import { Theme } from "@radix-ui/themes";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Issue Tracker",
   description: "Developed by Amirhossein Emadi",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -15,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable}`}>
       <body>
-        <Theme>
+        <Theme accentColor="violet" grayColor="sage" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
